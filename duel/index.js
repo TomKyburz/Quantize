36e47954364ee3a989aa8ac28adbc40ccc59749d
Tom Kyburz <115903065+TomKyburz@users.noreply.github.com>
2023-10-11 21:06:25 +0100

    Update index.js

--- duel/index.js
+++ duel/index.js
@@ -1,43 +1,43 @@
 const canvas = document.getElementById("canvas");
-      const c = canvas.getContext("2d");
-      const width = canvas.width;
-      const height = canvas.height;
+const c = canvas.getContext("2d");
+const width = canvas.width;
+const height = canvas.height;
 
-      // Player settings
-      const player = {
-        x: width / 2,
-        y: height / 2,
-        width: 30,
-        height: 30,
-        color: "red",
-        velocityY: 0,
-        gravity: 1,
-      };
+// Player settings
+const player = {
+  x: width / 2,
+  y: height / 2,
+  width: 30,
+  height: 30,
+  color: "red",
+  velocityY: 0,
+  gravity: 1,
+};
 
-      window.onload = () => {
-        const element = document.getElementById("game-container");
-        element.style.backgroundColor = "darkblue";
-        animate();
-      };
+window.onload = () => {
+  animate();
+};
 
-      function animate() {
-        // Clear the canvas
-        c.clearRect(0, 0, width, height);
+function animate() {
+  // Clear the canvas
+  c.clearRect(0, 0, width, height);
 
-        // Update player position
-        player.velocityY += player.gravity;
-        player.y += player.velocityY;
+  // Update player position
+  player.velocityY += player.gravity;
+  player.y += player.velocityY;
 
-        // Draw the player
-        c.fillStyle = player.color;
-        c.fillRect(player.x, player.y, player.width, player.height);
+  // Check if the player hits the bottom
+  if (player.y + player.height >= height) {
+    player.y = height - player.height;
+    player.velocityY = 0; // Stop when hitting the bottom
+  }
 
-        // Request animation frame
-        requestAnimationFrame(animate);
-      }
+  // Draw the player
+  c.fillStyle = player.color;
+  c.fillRect(player.x, player.y, player.width, player.height);
 
-      // You can also handle user input here to make the player jump or move left/right.
+  // Request animation frame
+  requestAnimationFrame(animate);
+}
 
-      function credits() {
-        window.location.href = "../credits.html";
-      }
+// You can add user input handling to make the player jump or move left/right.
